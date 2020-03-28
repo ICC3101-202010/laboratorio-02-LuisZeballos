9a0cd9bf36f6ab757e6164bb6722e7223eed62d8
LuisZeballos <lazeballos@miuandes.cl>
2020-03-28 18:26:07 -0300

    first commit

---
+++ Laboratorio 2/Laboratorio 2/Canción.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_2
+{
+    public class Canción
+    {
+        protected string nombre;
+        protected string artista;
+        protected string album;
+        protected string genero;
+
+        public string Informacion()
+        {
+            return ("Género: " + genero + ", Artista: " + artista + ", Álbum: " + album + ", Nombre: " + nombre);
+        }
+
+        public Canción(string aNombre, string aArtista, string aAlbum, string aGenero)
+        {
+            nombre = aNombre;
+            artista = aArtista;
+            album = aAlbum;
+            genero = aGenero;
+
+        }
+    }
+}
---
+++ Laboratorio 2/Laboratorio 2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Canción C1 = new Canción("Vete","Bad Bunny","YHLQMDLG","Reggaeton");
+            Canción C2 = new Canción("Diosa", "Myke Towers", "Easy Money Baby", "Reggaeton");
+            Canción C3 = new Canción("Lo malo de ser bueno", "El Cuarteto De Nos", "Porfiado", "Rock en español");
+
+            Console.WriteLine(C1.Informacion());
+            Console.WriteLine(C2.Informacion());
+            Console.WriteLine(C3.Informacion());
+            Console.ReadLine();
+        }
+    }
+}
